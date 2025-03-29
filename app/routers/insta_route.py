d44d197364ff6102532a6d8b2e109cc0ade08b8d
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-03-29 05:40:16 +0500

    check

--- app/routers/insta_route.py
+++ app/routers/insta_route.py
@@ -20,6 +20,7 @@ async def get_instagram_media(insta_data: InstaSchema = Form(...), db : AsyncSes
         .limit(1)  
     )
     _proxy = result.scalars().first()
+    print(_proxy.proxy, 'proxy')
     proxy_config = {    
         "server": f"http://{_proxy.proxy}",
         "username": _proxy.username,
