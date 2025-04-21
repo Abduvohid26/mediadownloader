b51264655a1c8d5d3555b914a12da53f37298dbf
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-21 16:27:37 +0500

    check

--- app/routers/tiktok_route.py
+++ app/routers/tiktok_route.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Form, HTTPException
 from fastapi.encoders import jsonable_encoder
-from schema.schema import YtSchema
+from schema.schema import TkSchema
 from .tiktok import download_from_snaptik
 tk_router = APIRouter()
 
@@ -8,6 +8,20 @@ tk_router = APIRouter()
 async def tk_media(tk_url: str):
     try:
         data = await download_from_snaptik(tk_url.strip())
+        print(data, "Data")
+        if not data:
+            return {"status": "error", "message": "Invalid response from the server."}
+        return data
+    except Exception as e:
+        print(f"Xatolik Yuz Berdi: {e}")
+        return {"status": "error", "message": "Invalid response from the server."}
+
+
+
+@tk_router.post("/tiktok/media/service/", include_in_schema=False)
+async def tk_media_service(url: TkSchema = Form(...)):
+    try:
+        data = await download_from_snaptik(url.url.strip())
         print(data, "Data")
         if not data:
             return {"status": "error", "message": "Invalid response from the server."}
--- app/schema/schema.py
+++ app/schema/schema.py
@@ -38,7 +38,21 @@ class YtSchema(BaseModel):
         "json_schema_extra": {
             "examples": [
                 {
-                    "url": "https://www.instagram.com/example.uz/"
+                    "url": "https://youtu.be/2Rys8Udrlus?si=MBdCS5EV_y57811A"
+                }
+            ]
+        }
+    }
+
+
+
+class TkSchema(BaseModel):
+    url: str
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "url": "https://vt.tiktok.com/ZSr9A2KyN/"
                 }
             ]
         }
