77a9ac445c9a8d3d7cf07a74b8cfebf8a78a7712
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-22 17:54:45 +0500

    check

--- app/routers/insta_route.py
+++ app/routers/insta_route.py
@@ -22,18 +22,30 @@ async def get_instagram_media(in_url: str, request: Request, db : AsyncSession =
     return media_urls
 
 @insta_router.post("/instagram/media/service/", include_in_schema=False)
-async def get_media(url: InstaSchema = Form(...)):
+async def get_media(request: Request, url: InstaSchema = Form(...)):
     url = url.url.strip()
+    context = request.app.state.context
     proxy_config = await get_proxy_config()
     if "stories" in url:
-        data =  await get_instagram_story_urls(url, proxy_config)
+        data =  await get_instagram_story_urls(url, context)
         return data
-    media_urls = await download_instagram_media(url, proxy_config)
+    media_urls = await download_instagram_media(url, proxy_config, context)
 
     if not media_urls:  
         return {"error": True, "message": "Invalid response from the server."}
 
     return media_urls
+    # url = url.url.strip()
+    # proxy_config = await get_proxy_config()
+    # if "stories" in url:
+    #     data =  await get_instagram_story_urls(url, proxy_config)
+    #     return data
+    # media_urls = await download_instagram_media(url, proxy_config)
+
+    # if not media_urls:  
+    #     return {"error": True, "message": "Invalid response from the server."}
+
+    # return media_urls
 
 
 
