7c470ddc94aa5a9b02651be5e5ede6ba542b4636
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-04-24 11:15:50 +0500

    check

--- app/routers/cashe.py
+++ app/routers/cashe.py
@@ -2,8 +2,8 @@ import os
 import redis
 
 
-redis_host = os.environ.get("REDIS_HOST", "redis_")
-redis_port = os.environ.get("REDIS_PORT", 6371)
+redis_host = os.environ.get("REDIS_HOST", "redis")
+redis_port = os.environ.get("REDIS_PORT", 6370)
 
 
 redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
