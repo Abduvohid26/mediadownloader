098cbb78b63c5a339ecb2bc1d2459f50cb9e1e12
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-05-06 12:19:25 +0500

    check

--- app/routers/yt_search/test_route.py
+++ app/routers/yt_search/test_route.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from .test import get_audio_direct_links
-
+import time
 test_route = APIRouter()
 
 
 @test_route.get("/test/search/")
 async def get_direct_links_route(query: str, limit: int = 10):
-    return  await get_audio_direct_links(query, limit)
+    curr_time = time.time()
+    data =  await get_audio_direct_links(query, limit)
+    print("time", time.time() - curr_time)
+    return data
