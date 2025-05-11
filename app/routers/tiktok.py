ebf12d5e95029a711b5c6e92b94c3844d50a5d1f
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-05-11 16:35:29 +0500

    check

--- app/routers/tiktok.py
+++ app/routers/tiktok.py
@@ -107,88 +107,95 @@ async def download_from_snaptik(url, request):
     start_time = time.time()
     """Snaptik orqali TikTok videolarini yuklab olish funksiyasi."""
     page = None
+    browser = None
+    playwright = None
 
-    async with request.app.state.restart_lock:
+    # async with request.app.state.restart_lock:
+    try:
+        page = await asyncio.wait_for(request.app.state.page_pool2.get(), timeout=5)
+    except asyncio.TimeoutError:
+        playwright = await async_playwright().start()
+        common_args = {'headless': True, 'args': ['--no-sandbox', '--disable-setuid-sandbox']}
+        browser = await playwright.chromium.launch(**common_args)
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto("https://snaptik.app", wait_until="load", timeout=10000)
+        # return {"error": True, "message": "Server band. Iltimos, keyinroq urinib ko‘ring."}
+    try:
+
+        consent = await page.query_selector('button:has-text("Consent")')
+        if consent:
+            await page.click('button:has-text("Consent")')
+        await page.wait_for_timeout(1000)
+        await page.mouse.click(10, 10)
+
+        await page.fill("input[name='url']", url)
+        await page.click("button[type='submit'][aria-label='Get']")
+        await page.wait_for_timeout(1000)
+
+        result = {
+            "error": False,
+            "shortcode": None,
+            "hosting": "tiktok",
+            "type": None,
+            "url": url,
+            "title": None,
+            "medias": []
+        }
+
+        # Title
+        try:
+            title_element = await page.query_selector(".video-title")
+            if title_element:
+                result["title"] = (await title_element.inner_text()).strip()
+        except Exception as e:
+            logger.warning(f"⚠️ Sarlavha topishda xatolik: {e}")
+
+        image_links  = await page.query_selector_all(".photo")
+        if not image_links:
+            thumb_url = None
             try:
-                
-                page = await asyncio.wait_for(request.app.state.page_pool2.get(), timeout=10)
-            except asyncio.TimeoutError:
-                return {"error": True, "message": "Server band. Iltimos, keyinroq urinib ko‘ring."}
-            try:
-
-                consent = await page.query_selector('button:has-text("Consent")')
-                if consent:
-                    await page.click('button:has-text("Consent")')
-                await page.wait_for_timeout(1000)   
-                await page.mouse.click(10, 10)
-                
-                await page.fill("input[name='url']", url)
-                await page.click("button[type='submit'][aria-label='Get']")
-                await page.wait_for_timeout(1000)
-
-                result = {
-                    "error": False,
-                    "shortcode": None,
-                    "hosting": "tiktok",
-                    "type": None,
-                    "url": url,
-                    "title": None,
-                    "medias": []
-                }
-
-                # Title
-                try:
-                    title_element = await page.query_selector(".video-title")
-                    if title_element:
-                        result["title"] = (await title_element.inner_text()).strip()
-                except Exception as e:
-                    logger.warning(f"⚠️ Sarlavha topishda xatolik: {e}")
-
-                image_links  = await page.query_selector_all(".photo")
-                if not image_links:
-                    thumb_url = None
-                    try:    
-                        thumb_img = await page.query_selector(".video-header img")
-                        if thumb_img:
-                            thumb_url = await thumb_img.get_attribute("src")
-                    except Exception as e:
-                        logger.warning(f"⚠️ Thumbnail topishda xatolik: {e}")
-
-                    try:
-                        video_links_divs = await page.query_selector_all(".video-links")
-                        for div in video_links_divs:
-                            a_tag = await div.query_selector("a")
-                            if a_tag:
-                                href = await a_tag.get_attribute("href")
-                                if href:
-                                    result["medias"].append({
-                                        "type": "video",
-                                        "download_url": href,
-                                        "thumb": thumb_url
-                                    })
-                                    result["type"] = "video" if len(result["medias"]) == 1 else "album"
-                    except Exception as e:
-                        logger.warning(f"⚠️ Video linklar topishda xatolik: {e}")
-                else:
-                    for image in image_links:
-                        img_tag = await image.query_selector("img")
-                        if img_tag:
-                            src = await img_tag.get_attribute("src")
-                            if src:
-                                result["medias"].append({
-                                    "type": "image",
-                                    "download_url": src,
-                                    "thumb": src
-                                })
-                                result["type"] = "image"
-                if not result["medias"]:
-                    return {"error": True, "message": "Hech qanday media topilmadi."}
-                print(time.time() - start_time, "Spend time")
-                return result
-
+                thumb_img = await page.query_selector(".video-header img")
+                if thumb_img:
+                    thumb_url = await thumb_img.get_attribute("src")
             except Exception as e:
-                logger.error(f"❌ Xatolik: {e}")
-                return {"error": True, "message": "Serverdan noto‘g‘ri javob oldik."}
+                logger.warning(f"⚠️ Thumbnail topishda xatolik: {e}")
+
+            try:
+                video_links_divs = await page.query_selector_all(".video-links")
+                for div in video_links_divs:
+                    a_tag = await div.query_selector("a")
+                    if a_tag:
+                        href = await a_tag.get_attribute("href")
+                        if href:
+                            result["medias"].append({
+                                "type": "video",
+                                "download_url": href,
+                                "thumb": thumb_url
+                            })
+                            result["type"] = "video" if len(result["medias"]) == 1 else "album"
+            except Exception as e:
+                logger.warning(f"⚠️ Video linklar topishda xatolik: {e}")
+        else:
+            for image in image_links:
+                img_tag = await image.query_selector("img")
+                if img_tag:
+                    src = await img_tag.get_attribute("src")
+                    if src:
+                        result["medias"].append({
+                            "type": "image",
+                            "download_url": src,
+                            "thumb": src
+                        })
+                        result["type"] = "image"
+        if not result["medias"]:
+            return {"error": True, "message": "Hech qanday media topilmadi."}
+        print(time.time() - start_time, "Spend time")
+        return result
+
+    except Exception as e:
+        logger.error(f"❌ Xatolik: {e}")
+        return {"error": True, "message": "Serverdan noto‘g‘ri javob oldik."}
 
             # try:
             #     try:
@@ -277,12 +284,14 @@ async def download_from_snaptik(url, request):
             #     logger.error(f"❌ Xatolik: {e}")
             #     return {"error": True, "message": "Serverdan noto‘g‘ri javob oldik."}
 
-            finally:
-                # Sahifani tozalab, navbatga qaytarish yoki yopish
-                if not page.is_closed():
-                    try:
-                        await page.reload()
-                        await request.app.state.page_pool2.put(page)
-                    except Exception as e:
-                        logger.warning(f"⚠️ Sahifani qayta navbatga qo‘yishda xatolik: {e}")
-                        await page.close()
+    finally:
+        await browser.close()
+        await playwright.stop()
+        # Sahifani tozalab, navbatga qaytarish yoki yopish
+        if not page.is_closed():
+            try:
+                await page.reload()
+                await request.app.state.page_pool2.put(page)
+            except Exception as e:
+                logger.warning(f"⚠️ Sahifani qayta navbatga qo‘yishda xatolik: {e}")
+                await page.close()
