9e02117079fe5ec6896152d7b8a83d38a5637f9c
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-05-12 16:23:11 +0500

    check

--- app/routers/tiktok_route.py
+++ app/routers/tiktok_route.py
@@ -2,14 +2,17 @@ from fastapi import APIRouter, Form, HTTPException, Request
 from fastapi.encoders import jsonable_encoder
 from schema.schema import TkSchema
 from .tiktok import download_from_snaptik
+import time
 tk_router = APIRouter()
 
 @tk_router.get("/tiktok/media/")
 async def tk_media(tk_url: str, request: Request):
+    start_time = time.time() 
     try:
         data = await download_from_snaptik(tk_url.strip(), request)
         if not data:
             return {"status": "error", "message": "Invalid response from the server."}
+        print(time.time() - start_time, "SPEND TIME")
         return data
     except Exception as e:
         print(f"Xatolik Yuz Berdi: {e}")
@@ -24,6 +27,6 @@ async def tk_media_service(request: Request, url: TkSchema = Form(...)):
         if not data:
             return {"status": "error", "message": "Invalid response from the server."}
         return data
-    except Exception as e:
+    except Exception as e:  
         print(f"Xatolik Yuz Berdi: {e}")
         return {"status": "error", "message": "Invalid response from the server."}
