c1768b2ce055fbeaf135463714b847b0be316812
abduvohid26 <abdujalilovabduvohid6@gmail.com>
2025-06-13 22:49:34 +0500

    check

--- app/routers/new_track/route.py
+++ app/routers/new_track/route.py
@@ -21,7 +21,18 @@ async def track_search(query: str, offset: int = 0, limit: int = 10):
 @new_track_router.post("/track/voice/search")
 async def track_voice_search(file: UploadFile = File(...), offset: int = 0, limit: int = 10):
     try:
-        return await track_recognize_by_multipart_reader(file)
+        proxy_config , proxy = await get_proxy_config(), None
+        if proxy_config:
+            proxy = f"http://{proxy_config['username']}:{proxy_config['password']}@{proxy_config['server'].replace('http://', '')}"
+        data =  await track_recognize_by_multipart_reader(file)
+        if data:
+            search_results = await track_backend_yt_dlp_search(f"{data['performer']} {data['title']}", int(offset), int(limit), proxy)
+            if search_results:
+                return {"search_results": search_results}
+            else:
+                return {"error": True, "message": "Error response from the server"}
+        else:
+            return {"error": True, "message": "Error response from the server"}
     except Exception as e:
         print("Xato:", e)
         return {"error": True, "message": "Error response from the server"}
